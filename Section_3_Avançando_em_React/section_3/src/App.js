272efebc4d6d697bca2d17b2a28cbdc434ebd950
unknown <arthurguilherme2013@gmail.com>
2022-11-02 12:15:25 +0000

    Renderização Condicional Simples

---
+++ Section_3_Avançando_em_React/section_3/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RenderizacaoCondicional from './components/RenderizacaoCondicional';
+
+function App() {
+  return (
+    <div className="App">
+      <RenderizacaoCondicional />
+    </div>
+  );
+}
+
+export default App;
