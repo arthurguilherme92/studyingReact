b19dc01d04e3e935a322dfab99b5a8a68d4632bc
unknown <arthurguilherme2013@gmail.com>
2022-11-07 12:35:38 +0000

    Introdução aos Props

--- Section_3_Avançando_em_React/section_3/src/App.js
+++ Section_3_Avançando_em_React/section_3/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import RenderizacaoCondicional from './components/RenderizacaoCondicional';
 import IfElseNoJSX from './components/IfElseNoJSX';
+import ShowUserName from './components/ShowUserName';
+import { useState } from 'react';
 
 function App() {
+  const [userName] = useState("Arthur Guilherme")
+
   return (
     <div className="App">
       <RenderizacaoCondicional />
       <IfElseNoJSX />
+      <ShowUserName name={userName}/>
     </div>
   );
 }
