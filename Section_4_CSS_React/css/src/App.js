451a2009a1caf1fca1393763e94253da50c1cbbd
unknown <arthurguilherme2013@gmail.com>
2022-12-20 12:54:56 +0000

    Studying CSS inline, CSS dinamic inline and Css dinamic classes

--- Section_4_CSS_React/css/src/App.js
+++ Section_4_CSS_React/css/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
 
+import {useState} from "react";
+
+
 function App() {
+  const n = 14;
+  const [nome] = useState("Arthur Guilherme")
+
   return (
     <div className="App">
       {/*CSS Global*/}
@@ -9,6 +15,14 @@ function App() {
       {/*CSS de Componente*/}
       <MyComponent />
       <p>Parágrafo do APP.JS</p>
+      {/*Inline CSS*/}
+      <p style={{color: "yellow", padding: "4rem", backgroundColor: "blue"}}>Este elemento foi estilizado de forma INLINE</p>
+      {/*Dinamic Inline CSS*/}
+      <h2 style={n < 8 ?{color: "red"} : {color: "green"}}>Css Dinâmico</h2>
+      <h2 style={n > 8 ?{color: "red"} : {color: "green"}}>Css Dinâmico</h2>
+      <h2 style={nome === "Arthur Guilherme" ?{color: "blue", backgroundColor: "white"} : null}>Teste Nome</h2>
+      {/*Dinamic Classes*/}
+      <h2 >Este título terá Classe dinâmica</h2>
     </div>
   );
 }
